a126f4101e092d89768e57054d8c0188bc9eae4a
DAVINDER PAL <dpsangwal@gmail.com>
2020-05-25 15:25:44 +0300

    added more functions for backup
    * added sha256 of backup files
    * added functions to remove duplicate lines

--- backup.py
+++ backup.py
@@ -2,6 +2,7 @@ import confluent_kafka
 from datetime import datetime
 import tarfile
 import os
+import hashlib
 
 class KBackup:
     def __init__(self):
@@ -11,10 +12,28 @@ class KBackup:
             'group.id': "Kafka-BackUp-Consumer-Group",
             'auto.offset.reset': 'earliest'
         }
-        self.TMPFILE = "/tmp/kafka-backup.bin"
+        self.BACKUP_DIR = "/tmp"
+        self.BACKUP_TMP_FILE = self.BACKUP_DIR + "/kafka-backup.bin"
 
-    # def backupMd5Sum():
-        # it should create md5sum file for each backup file
+    def __calculateSha256(self, file):
+        with open(file,"rb") as f:
+            return hashlib.sha256(f.read()).hexdigest();
+
+    def __createSha256OfBackupFile(self, file):
+        with open(file + ".sha256", "w") as f:
+            f.write(self.__calculateSha256(file))
+
+    def __writeDataToKafkaBinFile(self,msg,mode):
+        with open(self.BACKUP_TMP_FILE, mode) as f:
+            f.write(msg.value().decode('utf-8'))
+            f.write("\n")
+
+    def __createTarGz(self):
+        file = self.BACKUP_DIR + "/" + datetime.now().strftime("%Y%M%d-%H%M%S") + ".tar.gz"
+        _t = tarfile.open(file, "w:gz")
+        _t.add(self.BACKUP_TMP_FILE)
+        _t.close()
+        self.__createSha256OfBackupFile(file)
 
     def readFromTopic(self):
         _rt = confluent_kafka.Consumer(self.CONSUMERCONFIG)
@@ -23,35 +42,20 @@ class KBackup:
         count = 0
         while True:
             msg = _rt.poll(timeout=5.0)
-
             if msg is None:
                 continue
             if msg.error():
                 print(f"Consumer Error: {msg.error()}")
                 continue
 
-            print(f"Recieved Message: {msg.value().decode('utf-8')}")
-            print(f"Current Poll Count: {count}")
             if count == 0:
-                with open(self.TMPFILE, "a+") as f:
-                    f.write(msg.value().decode('utf-8'))
-                    f.write("\n")
+                self.__writeDataToKafkaBinFile(msg, "a+")
             if count > 0:
                 if count % 50 == 0:
-                    print(f"Creating Tar of Last 50 Messages")
-                    filename = datetime.now().strftime('%Y%M%d-%H%M%S') + '.tar.gz'
-                    _t = tarfile.open('/tmp/' + filename, "w:gz")
-                    _t.add(self.TMPFILE)
-                    _t.close()
-                    with open(self.TMPFILE, "w") as f:
-                        f.write(msg.value().decode('utf-8'))
-                        f.write("\n")
+                    self.__createTarGz()
+                    self.__writeDataToKafkaBinFile(msg, "w")
                 else:
-                    print("Appending Data to File")
-                    with open(self.TMPFILE, "a+") as f:
-                        f.write(msg.value().decode('utf-8'))
-                        f.write("\n")
-
+                    self.__writeDataToKafkaBinFile(msg, "a+")
             count += 1
 
         _rt.close()
