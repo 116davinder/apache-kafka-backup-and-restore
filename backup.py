d2b7c03ddcd6a28ad7a43f648e4f35a0d852f243
Davinder Pal <dpsangwal@gmail.com>
2022-09-21 00:04:35 +0300

    * refractored Kbackup and Krestore into separate files

--- backup.py
+++ backup.py
@@ -1,78 +1,8 @@
-import confluent_kafka
 import os
 import logging
 import threading
 from common import common
-
-
-class KBackup:
-    def __init__(self, config):
-        """initialize variables"""
-        self.BOOTSTRAP_SERVERS = config['BOOTSTRAP_SERVERS']
-        self.GROUP_ID = config['GROUP_ID']
-        self.TOPIC_NAME_LIST = config['TOPIC_NAMES']
-        self.BACKUP_DIR = os.path.join(config['FILESYSTEM_BACKUP_DIR'], self.TOPIC_NAME_LIST[0])
-        try:
-            self.NUMBER_OF_MESSAGE_PER_BACKUP_FILE = int(config['NUMBER_OF_MESSAGE_PER_BACKUP_FILE'])
-        except (ValueError, KeyError):
-            logging.error(
-                f"NUMBER_OF_MESSAGE_PER_BACKUP_FILE {str(config['NUMBER_OF_MESSAGE_PER_BACKUP_FILE'])} is not integer value"
-            )
-            self.NUMBER_OF_MESSAGE_PER_BACKUP_FILE = 1000
-            logging.info(f"NUMBER_OF_MESSAGE_PER_BACKUP_FILE is set to default value {self.NUMBER_OF_MESSAGE_PER_BACKUP_FILE}")
-        try:
-            self.NUMBER_OF_KAFKA_THREADS = config['NUMBER_OF_KAFKA_THREADS']
-        except KeyError:
-            self.NUMBER_OF_KAFKA_THREADS = 1
-        self.CONSUMERCONFIG = {
-            'bootstrap.servers': self.BOOTSTRAP_SERVERS,
-            'group.id': self.GROUP_ID,
-            'auto.offset.reset': 'earliest'
-        }
-        try:
-            self.LOG_LEVEL = config['LOG_LEVEL']
-        except KeyError:
-            self.LOG_LEVEL = logging.INFO
-
-        logging.debug("successful loading of kafka variables")
-
-    def backup(self):
-        _bt = confluent_kafka.Consumer(self.CONSUMERCONFIG)
-        _bt.subscribe(self.TOPIC_NAME_LIST)
-
-        for p in common.findNumberOfPartitionsInTopic(_bt.list_topics().topics[self.TOPIC_NAME_LIST[0]].partitions):
-            os.makedirs(os.path.join(self.BACKUP_DIR, str(p)), exist_ok=True)
-
-        count = 0
-        logging.info(f"started polling on {self.TOPIC_NAME_LIST[0]}")
-        while True:
-            msg = _bt.poll(timeout=1.0)
-            if msg is None:
-                logging.debug(f"waiting for new messages from topic {self.TOPIC_NAME_LIST[0]}")
-                continue
-            if msg.error():
-                logging.error(f"{msg.error()}")
-                continue
-            if msg.partition() is not None:
-                _tmp_file = os.path.join(self.BACKUP_DIR, str(msg.partition()), "current.bin")
-                _tar_location = os.path.join(self.BACKUP_DIR, str(msg.partition()))
-                _msg = common.decodeMsgToUtf8(msg)
-                if _msg is not None:
-                    if count == 0:
-                        common.writeDataToKafkaBinFile(_tmp_file, _msg, "a+")
-                    if count > 0:
-                        if count % self.NUMBER_OF_MESSAGE_PER_BACKUP_FILE == 0:
-                            common.createTarGz(_tar_location, _tmp_file)
-                            common.writeDataToKafkaBinFile(_tmp_file, _msg, "w")
-                            count = 0
-                        else:
-                            common.writeDataToKafkaBinFile(_tmp_file, _msg, "a+")
-            else:
-                logging.error("no partition found for message")
-
-            count += 1
-
-        _bt.close()
+from library.backup import KBackup
 
 
 def main():
---
+++ library/restore.py
@@ -0,0 +1,115 @@
+import confluent_kafka
+import time
+from common import common
+import os
+import logging
+
+
+class KRestore:
+
+    def __init__(self, config):
+        """initialize variables"""
+        self.BOOTSTRAP_SERVERS = config['BOOTSTRAP_SERVERS']
+        self.BACKUP_TOPIC_NAME = config['BACKUP_TOPIC_NAME']
+        self.RESTORE_TOPIC_NAME = config['RESTORE_TOPIC_NAME']
+        self.BACKUP_DIR = config['FILESYSTEM_BACKUP_DIR']
+
+        self.PRODUCERCONFIG = {
+            'bootstrap.servers': self.BOOTSTRAP_SERVERS,
+            'enable.idempotence': True,
+        }
+        try:
+            self.RETRY_SECONDS = config['RETRY_SECONDS']
+        except KeyError:
+            self.RETRY_SECONDS = 60
+
+        self.FILESYSTEM_TYPE = config['FILESYSTEM_TYPE']
+        self.FILESYSTEM_BACKUP_DIR = config['FILESYSTEM_BACKUP_DIR']
+
+        if self.FILESYSTEM_TYPE == "S3":
+            self.BUCKET_NAME = config['BUCKET_NAME']
+
+        elif self.FILESYSTEM_TYPE == "AZURE":
+            self.CONTAINER_NAME = config['CONTAINER_NAME']
+
+        try:
+            self.RESTORE_PARTITION_STRATEGY = config['RESTORE_PARTITION'].lower()
+        except KeyError:
+            self.RESTORE_PARTITION_STRATEGY = "random"
+
+        try:
+            self.LOG_LEVEL = config['LOG_LEVEL']
+        except KeyError:
+            self.LOG_LEVEL = logging.INFO
+
+        logging.debug("successful loading variables")
+
+    def delivery_report(err, msg):
+        if err is not None:
+            logging.error(f'Message delivery failed: {err}')
+        else:
+            logging.debug(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+
+    def write_to_kafka(rt,binFile,partition,topic,rts):
+        if binFile is not None:
+            with open(binFile) as _f:
+                for line in _f.readlines():
+                    line.strip()
+                    rt.poll(0)
+                    if rts == "random":
+                        rt.produce(
+                            topic,
+                            line.encode('utf-8'),
+                            callback=KRestore.delivery_report
+                        )
+                    elif rts == "same":
+                        rt.produce(
+                            topic,
+                            line.encode('utf-8'),
+                            callback=KRestore.delivery_report,
+                            partition=int(partition)
+                        )
+                    rt.flush()
+            try:
+                os.remove(binFile)
+            except FileNotFoundError:
+                pass
+
+    def restore(self):
+        _rt = confluent_kafka.Producer(self.PRODUCERCONFIG)
+        while True:
+            _base_topic_dir = os.path.join(self.BACKUP_DIR, self.BACKUP_TOPIC_NAME)
+            _partitions_in_backup_dir = common.listDirs(_base_topic_dir)
+            for _p in _partitions_in_backup_dir:
+                _partition_dir = os.path.join(_base_topic_dir, _p)
+                _partition_backup_files = common.findFilesInFolder(_partition_dir, pattern="*.tar.gz")
+
+                # restore already extracted files
+                _partition_backup_bin_files = common.findFilesInFolder(_partition_dir, pattern="*.bin")
+                for _r_bin_file in _partition_backup_bin_files:
+                    KRestore.write_to_kafka(
+                        _rt,
+                        _r_bin_file,
+                        _p,
+                        self.RESTORE_TOPIC_NAME,
+                        self.RESTORE_PARTITION_STRATEGY
+                    )
+                    logging.info(f"restore successful of file {_r_bin_file}.tar.gz")
+
+                # restore new .tar.gz files
+                for _file in _partition_backup_files:
+                    _file = str(_file)
+                    _sha_file = _file + ".sha256"
+                    if os.path.getsize(_file) > 0 and os.path.exists(_sha_file):
+                        _binFile = common.extractBinFile(_file, _sha_file, _partition_dir)
+                        KRestore.write_to_kafka(
+                            _rt,
+                            _binFile,
+                            _p,
+                            self.RESTORE_TOPIC_NAME,
+                            self.RESTORE_PARTITION_STRATEGY
+                        )
+                        logging.info(f"restore successful of file {_file}")
+
+            logging.info(f"retry for more files in {_base_topic_dir} after {self.RETRY_SECONDS}")
+            time.sleep(self.RETRY_SECONDS)
